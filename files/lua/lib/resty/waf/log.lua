74dea987e4ac15bf648b00eef2c75ccfe54ac857
Uriah Feng <uriahfeng@gmail.com>
2019-04-27 21:55:25 +0800

    init

---
+++ files/lua/lib/resty/waf/log.lua
@@ -0,0 +1,58 @@
+local _M = {}
+
+
+local cjson         = require "cjson"
+local socket_logger = require "resty.logger.socket"
+
+_M.version = "0.9"
+
+-- warn logger
+function _M.warn(waf, msg)
+	ngx.log(ngx.WARN, '[', waf.transaction_id, '] ', msg)
+end
+
+-- fatal failure logger
+function _M.fatal_fail(msg)
+	ngx.log(ngx.ERR, error(msg))
+	ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+end
+
+-- event log writer lookup table
+_M.write_log_events = {
+	error = function(waf, t)
+		ngx.log(waf._event_log_level, cjson.encode(t))
+	end,
+	file = function(waf, t)
+		if (not waf._event_log_target_path) then
+			_M.fatal_fail("Event log target path is undefined in file logger")
+		end
+
+		local f = io.open(waf._event_log_target_path, 'a')
+
+		if (not f) then
+			_M.warn(waf, "Could not open " .. waf._event_log_target_path)
+			return
+		end
+
+		f:write(cjson.encode(t), "\n")
+		f:close()
+	end,
+	socket = function(waf, t)
+		if (not socket_logger.initted()) then
+			socket_logger.init({
+				host           = waf._event_log_target_host,
+				port           = waf._event_log_target_port,
+				sock_type      = waf._event_log_socket_proto,
+				ssl            = waf._event_log_ssl,
+				ssl_verify     = waf._event_log_ssl_verify,
+				sni_host       = waf._event_log_ssl_sni_host,
+				flush_limit    = waf._event_log_buffer_size,
+				periodic_flush = waf._event_log_periodic_flush
+			})
+		end
+
+		socket_logger.log(cjson.encode(t) .. "\n")
+	end
+}
+
+return _M
---
+++ files/lua/lib/resty/waf/util.lua
@@ -0,0 +1,287 @@
+local _M = {}
+
+_M.version = "0.9"
+
+local cjson  = require "cjson"
+local logger = require "resty.waf.log"
+
+
+local string_byte   = string.byte
+local string_char   = string.char
+local string_format = string.format
+local string_gmatch = string.gmatch
+local string_match  = string.match
+local string_upper  = string.upper
+local table_concat  = table.concat
+
+-- duplicate a table using recursion if necessary for multi-dimensional tables
+-- useful for getting a local copy of a table
+function _M.table_copy(orig)
+	local orig_type = type(orig)
+	local copy
+
+	if (orig_type == 'table') then
+		copy = {}
+
+		for orig_key, orig_value in next, orig, nil do
+			copy[_M.table_copy(orig_key)] = _M.table_copy(orig_value)
+		end
+
+		setmetatable(copy, _M.table_copy(getmetatable(orig)))
+	else
+		copy = orig
+	end
+	return copy
+end
+
+-- return a table containing the keys of the provided table
+function _M.table_keys(table)
+	if (type(table) ~= "table") then
+		logger.fatal_fail(type(table) .. " was given to table_keys!")
+	end
+
+	local t = {}
+	local n = 0
+
+	for key, _ in pairs(table) do
+		n = n + 1
+		t[n] = tostring(key)
+	end
+
+	return t
+end
+
+-- return a table containing the values of the provided table
+function _M.table_values(table)
+	if (type(table) ~= "table") then
+		logger.fatal_fail(type(table) .. " was given to table_values!")
+	end
+
+	local t = {}
+	local n = 0
+
+	for _, value in pairs(table) do
+		-- if a table as a table of values, we need to break them out and add them individually
+		-- request_url_args is an example of this, e.g. ?foo=bar&foo=bar2
+		if (type(value) == "table") then
+			for _, values in pairs(value) do
+				n = n + 1
+				t[n] = tostring(values)
+			end
+		else
+			n = n + 1
+			t[n] = tostring(value)
+		end
+	end
+
+	return t
+end
+
+-- return true if the table key exists
+function _M.table_has_key(needle, haystack)
+	if (type(haystack) ~= "table") then
+		logger.fatal_fail("Cannot search for a needle when haystack is type " .. type(haystack))
+	end
+
+	return haystack[needle] ~= nil
+end
+
+-- determine if the haystack table has a needle for a key
+function _M.table_has_value(needle, haystack)
+	if (type(haystack) ~= "table") then
+		logger.fatal_fail("Cannot search for a needle when haystack is type " .. type(haystack))
+	end
+
+	for _, value in pairs(haystack) do
+		if (value == needle) then
+			return true
+		end
+	end
+
+	return false
+end
+
+-- pick out dynamic data from storage key definitions
+function _M.parse_dynamic_value(waf, key, collections)
+	local lookup = function(m)
+		local val      = collections[string_upper(m[1])]
+		local specific = m[2]
+
+		if (not val) then
+			logger.fatal_fail("Bad dynamic parse, no collection key " .. m[1])
+		end
+
+		if (type(val) == "table") then
+			if (specific) then
+				return tostring(val[specific])
+			else
+				return m[1]
+			end
+		else
+			return val
+		end
+	end
+
+	-- grab something that looks like
+	-- %{VAL} or %{VAL.foo}
+	-- and find it in the lookup table
+	local str = ngx.re.gsub(key, [[%{([A-Za-z_]+)(?:\.([^}]+))?}]], lookup, waf._pcre_flags)
+
+	if waf._debug == true then ngx.log(waf._debug_log_level, '[', waf.transaction_id, '] ', "Parsed dynamic value is " .. str) end
+
+	if (ngx.re.find(str, [=[^\d+$]=], waf._pcre_flags)) then
+		return tonumber(str)
+	else
+		return str
+	end
+end
+
+-- safely attempt to parse a JSON string as a ruleset
+function _M.parse_ruleset(data)
+	local jdata
+
+	if pcall(function() jdata = cjson.decode(data) end) then
+		return jdata, nil
+	else
+		return nil, "could not decode " .. data
+	end
+end
+
+-- find a rule file with a .json suffix, read it, and return a JSON string
+function _M.load_ruleset_file(name)
+	for k, v in string_gmatch(package.path, "[^;]+") do
+		local path = string_match(k, "(.*/)")
+
+		local full_name = path .. "rules/" .. name .. ".json"
+
+		local f = io.open(full_name)
+		if (f ~= nil) then
+			local data = f:read("*all")
+
+			f:close()
+
+			return _M.parse_ruleset(data)
+		end
+	end
+
+	return nil, "could not find " .. name
+end
+
+-- encode a given string as hex
+function _M.hex_encode(str)
+	return (str:gsub('.', function (c)
+		return string_format('%02x', string_byte(c))
+	end))
+end
+
+-- decode a given hex string
+function _M.hex_decode(str)
+	local value
+
+	if (pcall(function()
+		value = str:gsub('..', function (cc)
+			return string_char(tonumber(cc, 16))
+		end)
+	end)) then
+		return value
+	else
+		return str
+	end
+end
+
+-- build an RBLDNS query by reversing the octets of an IPv4 address and prepending that to the rbl server name
+function _M.build_rbl_query(ip, rbl_srv)
+	if (type(ip) ~= 'string') then
+		return false
+	end
+
+	local o1, o2, o3, o4 = ip:match("(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)")
+
+	if (not o1 and not o2 and not o3 and not o4) then
+		return false
+	end
+
+	local t = { o4, o3, o2, o1, rbl_srv }
+
+	return table_concat(t, '.')
+end
+
+-- parse collection elements based on a given directive
+_M.parse_collection = {
+	specific = function(waf, collection, value)
+		if waf._debug == true then ngx.log(waf._debug_log_level, '[', waf.transaction_id, '] ', "Parse collection is getting a specific value: " .. value) end
+		return collection[value]
+	end,
+	regex = function(waf, collection, value)
+		if waf._debug == true then ngx.log(waf._debug_log_level, '[', waf.transaction_id, '] ', "Parse collection is geting for the regex: " .. value) end
+		local v
+		local n = 0
+		local _collection = {}
+		for k, _ in pairs(collection) do
+			if waf._debug == true then ngx.log(waf._debug_log_level, '[', waf.transaction_id, '] ', "checking " .. k) end
+			if (ngx.re.find(k, value, waf._pcre_flags)) then
+				v = collection[k]
+				if (type(v) == "table") then
+					for __, _v in pairs(v) do
+						n = n + 1
+						_collection[n] = _v
+					end
+				else
+					n = n + 1
+					_collection[n] = v
+				end
+			end
+		end
+		return _collection
+	end,
+	ignore_regex = function(waf, collection, value)
+		local v
+		local n = 0
+		local _collection = {}
+		for k, _ in pairs(collection) do
+			if (not ngx.re.find(k, value, waf._pcre_flags)) then
+				v = collection[k]
+				if (type(v) == "table") then
+					for __, _v in pairs(v) do
+						n = n + 1
+						_collection[n] = _v
+					end
+				else
+					n = n + 1
+					_collection[n] = v
+				end
+			end
+		end
+		return _collection
+	end,
+	ignore = function(waf, collection, value)
+		if waf._debug == true then ngx.log(waf._debug_log_level, '[', waf.transaction_id, '] ', "Parse collection is ignoring a value: " .. value) end
+		local _collection = {}
+		_collection = _M.table_copy(collection)
+		_collection[value] = nil
+		return _collection
+	end,
+	keys = function(waf, collection)
+		if waf._debug == true then ngx.log(waf._debug_log_level, '[', waf.transaction_id, '] ', "Parse collection is getting the keys") end
+		return _M.table_keys(collection)
+	end,
+	values = function(waf, collection)
+		if waf._debug == true then ngx.log(waf._debug_log_level, '[', waf.transaction_id, '] ', "Parse collection is getting the values") end
+		return _M.table_values(collection)
+	end,
+	all = function(waf, collection)
+		local n = 0
+		local _collection = {}
+		for _, key in ipairs(_M.table_keys(collection)) do
+			n = n + 1
+			_collection[n] = key
+		end
+		for _, value in ipairs(_M.table_values(collection)) do
+			n = n + 1
+			_collection[n] = value
+		end
+		return _collection
+	end
+}
+
+return _M
